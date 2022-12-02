ed7d5ac5c6f84267e2b5963a0a84506dc2d4b1f6
ice.breaker <ice.breaker@trigram.co>
2022-12-02 11:13:49 -0600

    chore: Move 'actual uploading' tests into another file, make another script to run those

--- test/actual-upload.test.js
+++ test/actual-upload.test.js
@@ -10,175 +10,7 @@ import { encodeBase64, sleep } from './utils.js'
 require('dotenv').config()
 
 describe('Signer', function () {
-  describe('#sign()', function () {
-    beforeEach((context) => {
-      context.signer = new SigV4({
-        accessKeyId: 'id',
-        region: 'eu-central-1',
-        secretAccessKey: 'secret',
-      })
-    })
-
-    it('should sign', function ({ signer }) {
-      const url = signer.sign({
-        bucket: 'bucket-name',
-        key: 'name',
-      })
-
-      assert.equal(url.host, 'bucket-name.s3.eu-central-1.amazonaws.com')
-      assert.equal(url.pathname, '/name')
-      const search = url.searchParams
-      assert.equal(search.get('X-Amz-Expires'), '86400')
-      assert.equal(search.get('X-Amz-Algorithm'), 'AWS4-HMAC-SHA256')
-      assert.match(search.get('X-Amz-Credential') || '', /aws4_request/)
-      assert.equal(search.get('X-Amz-SignedHeaders'), 'host')
-      assert.ok(typeof search.get('X-Amz-Signature') === 'string')
-    })
-
-    it('should sign with checksum', function ({ signer }) {
-      const url = signer.sign({
-        bucket: 'bucket-name',
-        key: 'name',
-        checksum: 'sss',
-      })
-
-      const search = url.searchParams
-      assert.equal(
-        search.get('X-Amz-SignedHeaders'),
-        'host;x-amz-checksum-sha256'
-      )
-    })
-
-    it('should sign with expires', function ({ signer }) {
-      const url = signer.sign({
-        bucket: 'bucket-name',
-        key: 'name',
-        checksum: 'sss',
-        expires: 1000,
-      })
-
-      const search = url.searchParams
-      assert.equal(search.get('X-Amz-Expires'), '1000')
-    })
-
-    it('should sign with session token when given', function ({ signer }) {
-      const token = 'token_123'
-      const url = signer.sign({
-        bucket: 'bucket-name',
-        key: 'name',
-        checksum: 'sss',
-        expires: 1000,
-        sessionToken: token,
-      })
-
-      const search = url.searchParams
-      expect(search.get('X-Amz-Security-Token')).toBe(token)
-    })
-
-    it('should sign NOT with session token when NOT given', function ({
-      signer,
-    }) {
-      const url = signer.sign({
-        bucket: 'bucket-name',
-        key: 'name',
-        checksum: 'sss',
-        expires: 1000,
-      })
-
-      const search = url.searchParams
-      expect(search.get('X-Amz-Security-Token')).toBeNull()
-    })
-
-    it('should sign with public read when true', function ({ signer }) {
-      const url = signer.sign({
-        bucket: 'bucket-name',
-        key: 'name',
-        checksum: 'sss',
-        expires: 1000,
-        publicRead: true,
-      })
-
-      const search = url.searchParams
-      expect(search.get('x-amz-acl')).toBe('public-read')
-    })
-
-    it('should sign NOT with public read when not given', function ({
-      signer,
-    }) {
-      const url = signer.sign({
-        bucket: 'bucket-name',
-        key: 'name',
-        checksum: 'sss',
-        expires: 1000,
-      })
-
-      const search = url.searchParams
-      expect(search.get('x-amz-acl')).toBeNull()
-    })
-
-    it('should sign with size when given', function ({ signer }) {
-      const url = signer.sign({
-        bucket: 'bucket-name',
-        key: 'name',
-        checksum: 'sss',
-        expires: 1000,
-        contentLength: 1024,
-      })
-
-      const search = url.searchParams
-      assert.equal(
-        search.get('X-Amz-SignedHeaders'),
-        'content-length;host;x-amz-checksum-sha256'
-      )
-    })
-
-    //     it('should sign with int size when given float', function ({ signer }) {
-    //       const url = signer.sign({
-    //         bucket: 'bucket-name',
-    //         key: 'name',
-    //         checksum: 'sss',
-    //         expires: 1000,
-    //         contentLength: 1024.213,
-    //       })
-    //
-    //       const search = url.searchParams
-    //       console.log('search', search)
-    //       expect(search.get('Content-Length')).toBe('1024')
-    //     })
-
-    it('should NOT sign with size when size is 0', function ({ signer }) {
-      const url = signer.sign({
-        bucket: 'bucket-name',
-        key: 'name',
-        checksum: 'sss',
-        expires: 1000,
-        contentLength: 0,
-      })
-
-      const search = url.searchParams
-      assert.equal(
-        search.get('X-Amz-SignedHeaders'),
-        'host;x-amz-checksum-sha256'
-      )
-    })
-
-    it('should NOT sign with size when not given', function ({ signer }) {
-      const url = signer.sign({
-        bucket: 'bucket-name',
-        key: 'name',
-        checksum: 'sss',
-        expires: 1000,
-      })
-
-      const search = url.searchParams
-      assert.equal(
-        search.get('X-Amz-SignedHeaders'),
-        'host;x-amz-checksum-sha256'
-      )
-    })
-  })
-
-  describe.skip('s3 integration needs .env and cors setup', function () {
+  describe('s3 integration needs .env and cors setup', function () {
     beforeEach((context) => {
       context.data = { key: 'value' }
 
@@ -494,69 +326,68 @@ describe('Signer', function () {
         req.end()
       })
     })
-  })
-
-  it('should sign and upload, "manual writing with socket".', async function ({
-    signer,
-    hash,
-    data,
-    bucket,
-  }) {
-    const content = JSON.stringify(data)
-    const contentLength = Buffer.from(content).byteLength
-    const url = signer.sign({
+    it('should sign and upload, "manual writing with socket".', async function ({
+      signer,
+      hash,
+      data,
       bucket,
-      key: `testing/test-file-${Date.now()}-invalid-size.json`,
-      checksum: hash,
-      expires: 1000,
-      contentLength: contentLength,
+    }) {
+      const content = JSON.stringify(data)
+      const contentLength = Buffer.from(content).byteLength
+      const url = signer.sign({
+        bucket,
+        key: `testing/test-file-${Date.now()}-invalid-size.json`,
+        checksum: hash,
+        expires: 1000,
+        contentLength: contentLength,
+      })
+
+      //       const rsp = await badFetchSocket(new URL('http://127.0.0.1'), {
+      const rsp = await badFetchSocket(url, {
+        method: 'PUT',
+        body: content,
+        contentEncoding: 'base64',
+        contentType: 'application/json',
+        headers: {
+          'content-length': contentLength, //actual size is 15
+          'x-amz-checksum-sha256': hash,
+        },
+      })
+
+      console.log(Buffer.from(rsp).toString())
+      assert.ok(rsp.statusCode != 200)
     })
 
-    //       const rsp = await badFetchSocket(new URL('http://127.0.0.1'), {
-    const rsp = await badFetchSocket(url, {
-      method: 'PUT',
-      body: content,
-      contentEncoding: 'base64',
-      contentType: 'application/json',
-      headers: {
-        'content-length': contentLength, //actual size is 15
-        'x-amz-checksum-sha256': hash,
-      },
-    })
-
-    console.log(Buffer.from(rsp).toString())
-    assert.ok(rsp.statusCode != 200)
-  })
-
-  it('should sign and fail upload when content size is smaller, "manual writing with socket".', async function ({
-    signer,
-    hash,
-    data,
-    bucket,
-  }) {
-    const content = JSON.stringify(data)
-    const contentLength = Buffer.from(content).byteLength
-    const url = signer.sign({
+    it('should sign and fail upload when content size is smaller, "manual writing with socket".', async function ({
+      signer,
+      hash,
+      data,
       bucket,
-      key: `testing/test-file-${Date.now()}-invalid-size.json`,
-      checksum: hash,
-      expires: 1000,
-      contentLength: contentLength - 5,
-    })
+    }) {
+      const content = JSON.stringify(data)
+      const contentLength = Buffer.from(content).byteLength
+      const url = signer.sign({
+        bucket,
+        key: `testing/test-file-${Date.now()}-invalid-size.json`,
+        checksum: hash,
+        expires: 1000,
+        contentLength: contentLength - 5,
+      })
 
-    //       const rsp = await badFetchSocket(new URL('http://127.0.0.1'), {
-    const rsp = await badFetchSocket(url, {
-      method: 'PUT',
-      body: content,
-      contentEncoding: 'base64',
-      contentType: 'application/json',
-      headers: {
-        'content-length': contentLength - 5, //actual size is 15
-        'x-amz-checksum-sha256': hash,
-      },
-    })
+      //       const rsp = await badFetchSocket(new URL('http://127.0.0.1'), {
+      const rsp = await badFetchSocket(url, {
+        method: 'PUT',
+        body: content,
+        contentEncoding: 'base64',
+        contentType: 'application/json',
+        headers: {
+          'content-length': contentLength - 5, //actual size is 15
+          'x-amz-checksum-sha256': hash,
+        },
+      })
 
-    console.log(Buffer.from(rsp).toString())
-    assert.ok(rsp.statusCode != 200)
+      console.log(Buffer.from(rsp).toString())
+      assert.ok(rsp.statusCode != 200)
+    })
   })
 })
